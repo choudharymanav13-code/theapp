b81219c95d52d5719b26bc313c9862d05553f524
choudharymanav13-code <choudharymanav13@gmail.com>
2025-09-13 18:34:20 +0530

    Update page.js

--- src/app/inventory/page.js
+++ src/app/inventory/page.js
@@ -1,7 +1,7 @@
 
 'use client'
 import { useEffect, useState } from 'react'
-import { supabase } from '@/src/lib/supabaseClient'
+import { supabase } from '../../lib/supabaseClient'
 export default function Inventory(){
   const [items, setItems] = useState([])
   const load = async ()=>{
